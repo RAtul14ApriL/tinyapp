5c7203642f95b5fd10ca97e4f6930691cfb6c036
Md Hefzul Bari <ratul14april@gmail.com>
2021-12-06 18:54:37 -0600

    Adding url Database

---
+++ express_server.js
@@ -0,0 +1,21 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running...`);
+});
